b71ac329df7340c11701b072c8b6042f9c5df78f
elviraghibner <elvira.ghibner@gmail.com>
2024-02-19 14:24:18 +0100

    update 19.02.24

--- elviraghibner/table_transactions.sql
+++ elviraghibner/table_transactions.sql
@@ -3,7 +3,7 @@ create table if not exists txs (
     transaction_id int primary key auto_increment, 
     from_account_id int,
     to_account_id int,
-    amount double,
+    amount float not null check(amount > 0),
     transaction_date timestamp default current_timestamp,
     comment varchar(128) not null
     foreign key (from_account_id) references accounts(id),

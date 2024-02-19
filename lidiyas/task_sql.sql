1907d182ac72bfa41ea5355d6d3eddb2fa01c0f8
sulida <sulida2011@gmail.com>
2024-02-19 14:55:36 +0200

    Correcred code

--- lidiyas/task_sql.sql
+++ lidiyas/task_sql.sql
@@ -1,9 +1,9 @@
 create table txs (
-id int,
-created_at timestamp,
-amount int,
-is_approved bool,
-sender_account_id int,
-recipient_account_id int
+id int primary key auto_increment,
+created_at timestamp default current_timestamp,
+amount double not null,
+is_approved bool default false,
+sender_account_id int not null,
+recipient_account_id int not null
 
 );

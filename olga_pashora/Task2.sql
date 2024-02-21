571f63276cb65749baab384913e3cc02b6c19619
Olga_Pashora <olga.pashora@gmail.com>
2024-02-21 12:39:40 +0100

    select orders

---
+++ olga_pashora/Task2.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Orders
+Where ShipperID = 1
+
+SELECT * FROM Orders
+join Shippers on Shippers.ShipperID = Orders.ShipperID
+Where Shippers.ShipperName = "Speedy Express"
+Group By Orders.OrderID
